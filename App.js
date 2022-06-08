b11d3f18c9ca7754ab13426d513d2473d3f91f50
chinmay <chinmaymhatre12@gmail.com>
2022-06-08 09:38:00 +0530

    fin

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {  FormControl,
+          Input,
+          Stack,
+          WarningOutlineIcon,
+          Box,
+          Center,
+          NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Box
+        w={{
+          base: "90%",
+          md: "25%",
+        }}
+        >
+          <FormControl isRequired>
+            <Stack mx="4">
+              <FormControl.Label>UserName</FormControl.Label>
+              <Input type="text" value="john doe" placeholder="User Name" />
+              <FormControl.HelperText>
+                Must be atleast 6 characters.
+              </FormControl.HelperText>
+              <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                Atleast 6 characters are required.
+              </FormControl.ErrorMessage>
+            </Stack>
+          </FormControl>
+          <FormControl isRequired>
+            <Stack mx="4">
+              <FormControl.Label>Password</FormControl.Label>
+              <Input type="password" value="12345" placeholder="password" />
+              <FormControl.HelperText>
+                Must be atleast 6 characters.
+              </FormControl.HelperText>
+              <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                Atleast 6 characters are required.
+              </FormControl.ErrorMessage>
+            </Stack>
+          </FormControl>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
